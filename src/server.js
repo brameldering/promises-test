1f9862f1c0f2815c24489c688be75062969008b6
brameldering <bram.eldering@gmail.com>
2023-06-20 08:28:49 +0200

    callback hell

--- src/server.js
+++ src/server.js
@@ -1,12 +1,33 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import superagent from "superagent";
 import express from "express";
 
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+fs.readFile(`${__dirname}/../dog.txt`, (err, data) => {
+  if (err) return console.log("error " + err.message);
+  console.log(`Breed: ${data}`);
+
+  superagent.get(`https://dog.ceo/api/breed/${data}/images/random`).end((err, res) => {
+    if (err) return console.log("error " + err.message);
+    console.log(res.body.message);
+    fs.writeFile("dog-img.txt", res.body.message, (err) => {
+      if (err) return console.log("error " + err.message);
+      console.log("Random img saved");
+    });
+  });
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`);
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}!`);
+// });
